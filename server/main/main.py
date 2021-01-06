9f7d162ecbee52c9bb222651103ef2df7de6ade5
Kryštof Woldřich <krystof51@gmail.com>
2021-01-06 19:14:48 +0100

    Add custom json encoder for mongo object id

--- server/main/main.py
+++ server/main/main.py
@@ -4,6 +4,7 @@ from api.security import security_namespace as security_resources
 from api.user import user_namespace as user_resource
 from api.heating import heating_namespace as heating_resource
 from api.heating_schedule import heating_schedule_namespace as heating_schedule_resource
+from database.jsonEncoder import JSONEncoder
 
 from parameters import config
 
@@ -22,6 +23,8 @@ api = Api(app,
 		title = config['api']['name'],
 		description = config['api']['description'],
 		authorizations=authorizations)
+
+app.config["RESTPLUS_JSON"] = {"cls": JSONEncoder}
 
 api.decorators = [cors.crossdomain(
     origin='*', headers=['accept', '*']
